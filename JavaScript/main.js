6a191a535d1f8b48fb55ef75c7b9b6fd2ec42f96
Rafael-Cabrera <rafael_cabrera@protegeseguros.com.sv>
2024-08-29 06:44:04 -0600

    Clase javascript 2024-08-28

---
+++ JavaScript/main.js
@@ -0,0 +1,26 @@
+console.log(1);
+console.log("Hola");
+console.log(true);
+
+/*
+VAR
+- Las variables son redefinibles
+- Scope - alcance
+
+*/
+var nombreVariable = false;
+var nombre = "Rafa";
+var nombre = "Antonio";
+console.log(nombre);
+
+/*
+LET
+- No son redeclarables
+- Son mutables
+*/
+let numero = 1;
+console.log(numero);
+
+
+const pi = 3.14159;
+console.log(pi);
