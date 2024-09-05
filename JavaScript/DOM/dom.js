016aead7a6dd2f653406c35e0caf832e4bc3a88e
Rafael-Cabrera <rafael_cabrera@protegeseguros.com.sv>
2024-09-05 07:28:48 -0600

    Practia: funcion multiplos de tres.

--- JavaScript/DOM/dom.js
+++ JavaScript/DOM/dom.js
@@ -14,3 +14,25 @@ console.log(elementoDOM.innerText);
 elementoDOM.innerText = "Te cambie desde el js";
 console.log(elementoDOM.innerHTML);
 elementoDOM.innerHTML = "<span>cambie ogtra vez<span>";
+console.log(elementoDOM.innerHTML);
+
+// Agregar otro elemento del HTML
+const contenido = document.querySelector("#content");
+
+console.log(contenido);
+contenido.innerHTML = '<article><h1>Ingresado desde JS</h1><h2>y yo soy su hermano</h2></article>';
+
+/*
+realiza una function que reciba un array numerico y que retorne
+un array completamente nuevo con los numeros que sean multiplos de 3
+y que la suma de todos sus numeros sea menor que 100
+*/
+
+
+// Agarramos el botón
+const btnApretable = document.getElementById('btn-magic');
+
+btnApretable.addEventListener('click',() => {
+    alert('Este mensaje es la mágia del botón!!');
+    console.log('despues del clic');
+});
---
+++ JavaScript/practicas/multiplodetres.js
@@ -0,0 +1,35 @@
+function fMultiplosDeTres(arr) {
+    //Verificar que el arreglo tenga datos
+    if (arr.length <= 0) {
+        return arr;
+    }
+
+    const aResult = []; // Inicializa el array resultante
+    let sum = 0; //Guarda la suma cada número
+    let num = 0; //Tomara el número en el índice del arreglo.
+    let limite = 100; //Límite de la sumatoria.
+
+    // Recorre el array
+    for (let i = 0; i < arr.length; i++) {
+        num = arr[i];
+        // Verificar si el número es mutiplo de 3
+        if ((num => num % 3 === 0) && num > 1) {
+            // Verificar si añadir el número a la suma no excede el límite de 100
+            if (sum + num < limite) {
+                aResult.push(num);
+                sum += num;
+            } else {
+                // Si la suma excede el límite, termina el bucle
+                break;
+            }
+        }
+    }
+
+    return aResult;
+}
+
+// Ejemplo de uso:
+const numArray = [1, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33];
+const resultadoArray = fMultiplosDeTres(numArray);
+console.log('Array resultante:');
+console.log(resultadoArray);  // Salida: [3, 6, 9, 12, 15, 18, 21] el 24 y otros ya no se agrega porque supera el límite de 100.
