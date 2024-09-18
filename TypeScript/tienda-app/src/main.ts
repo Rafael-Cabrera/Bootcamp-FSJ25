9caafcccd35d09f14a0c71ae5f8e8c33a714902c
Rafael-Cabrera <rafael_cabrera@protegeseguros.com.sv>
2024-09-17 20:33:35 -0600

    export e import de clases

--- TypeScript/tienda-app/src/main.ts
+++ TypeScript/tienda-app/src/main.ts
@@ -1,27 +1,52 @@
+import { Producto } from './clases/Producto';
 import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <h1>Hola desde MainTS</h1>
-    <form id="loginForm">
+    <h1>Gestion de Tiendita</h1>
+
+    <form id="formProducto">
       <section>
-        <label>Nombre de usurio:</label>
-        <input type="text" id="username" name="name" placeholrder="Ingresa tu nombre de usuario">
-      </section>
-      <section>
-        <label>Password:</label>
-        <input type="text" id="password" name="password" placeholder="Ingresa tu password">
+        <label>Nombre de producto:</label>
+        <input type="text" id="nombre" name="nombre" placeholrder="Ingresa el nombre del producto">
       </section>
 
-      <button type="submit">Iniciar sesion</button>
+      <section>
+        <label>Precio:</label>
+        <input type="text" id="precio" name="precio" placeholder="Ingresa el precio">
+      </section>
+
+      <section>
+        <label>Cantidad:</label>
+        <input type="text" id="cantidad" name="cantidad" placeholder="Ingresa la cantidad">
+      </section>
+      
+      <button type="submit">Agregar producto</button>
     </form>
   </div>
 `
 
 //document.querySelector<HTMLElement>('#parrafo')!.innerText = "Esto es texto desde el p";
-const form = document.getElementById("loginForm") as HTMLFormElement;
+const form = document.getElementById("formProducto") as HTMLFormElement;
 
 form.addEventListener("submit", (e:SubmitEvent)=> {
   e.preventDefault();
   console.log('Holis!!');
+
+  const id = Date.now();
+  //Seleccionamos los elementos a controlar  y retiraos su valor
+  const nombre = (document.getElementById('nombre') as HTMLInputElement).value;
+  const precio = parseFloat((document.getElementById('precio') as HTMLInputElement).value);
+  const cantidad = parseInt((document.getElementById('cantidad') as HTMLInputElement).value);
+
+  console.log(id);
+  console.log(nombre);
+  console.log(precio);
+  console.log(cantidad);
+
+  let productito = new Producto(1, nombre, precio, cantidad);
+  console.log(productito);
+
+  localStorage.setItem('productos',JSON.stringify(productito));
+
 });
