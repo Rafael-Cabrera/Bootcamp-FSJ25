4e4d173bdf0eb8c3b1fbfe05c4d3047997bf5082
Rafael-Cabrera <rafael_cabrera@protegeseguros.com.sv>
2024-09-25 21:08:01 -0600

    Primer hook

--- React/concepts-app/src/components/Saludo.jsx
+++ React/concepts-app/src/components/Saludo.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
 // Declaracion de un componente
 export const Saludo = ()=>{
+  //let nombre = "Rafa";
+
+  //Primer HOOK=> useState
+  //Hook -> es una funcion prehecha para realizar x accion
+  //useState(valor inicial)
+  const [nombre, cambiarNombre] = useState("Rafa");
+
     return (
       <>
         <h3>Yo realmente estoy en el compenente saludo jejex</h3>
+        <h2>Hola {nombre},cómo estás?</h2>
+        <button onClick={()=>{ cambiarNombre("Rafaelito") }}>Magia</button>
+        
       </>
     )
   }
